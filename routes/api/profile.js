356e8026f13b1ef964680a6e48a5006342adf4be
twilight-warlock <devansh.ts@somaiya.edu>
2020-07-18 05:17:14 +0530

    Created route to add user's experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -160,4 +160,51 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/profile/experience
+// @desc    Adds profile Experience
+// @access  Private
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company is required").not().isEmpty(),
+      check("from", "from date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    // Getting data from req.body
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      desciption,
+    } = req.body;
+
+    // Storing it in an object
+    const exp = { title, company, location, from, to, current, desciption };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(exp);
+
+      // Saving the experince to the profile
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
