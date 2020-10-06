855944f180431e60f1336eecd6d0a6ff1f302b3d
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-07 01:54:22 +0530

    Added route to comment on a post

--- routes/api/post.js
+++ routes/api/post.js
@@ -99,6 +99,8 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+// ---------------------------- Likes ---------------------------------
+
 // @route   Put api/post/like/:id
 // @desc    likes a post
 // @access  Private
@@ -132,12 +134,12 @@ router.put("/unlike/:id", auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    // Check for like
+    // Check for like before unliking
     if (
       post.likes.filter((like) => like.user.toString() === req.user.id)
         .length === 0
     ) {
-      return res.status(400).json({ msg: "Post already liked" });
+      return res.status(400).json({ msg: "Post not liked" });
     }
 
     // remove index
@@ -155,4 +157,42 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// ----------------------------- Comments ----------------------------------
+
+// @route   POST api/post/comment/:id
+// @desc    Creates a comment
+// @access  Private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
