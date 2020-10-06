97c85f7e10d88fdcb64b46bcb5b651a705628053
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-06 20:24:02 +0530

    Added route to add new post

--- models/Post.js
+++ models/Post.js
@@ -16,11 +16,39 @@ const PostSchema = new mongoose.Schema({
     type: String,
   },
   likes: [
-    (user: {
+    {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "users",
-    }),
+    }
+    }
   ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+      type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date:{
+        type: Date,
+        default: Date.now,
+      },
+    }
+  ],
+  date:{
+        type: Date,
+        default: Date.now,
+      },
 });
 
 module.exports = Posts = mongoose.model("post", PostSchema);
--- routes/api/post.js
+++ routes/api/post.js
@@ -1,9 +1,45 @@
 const express = require("express"),
-  router = express.Router();
+  router = express.Router(),
+  { check, validationResult } = require("express-validator"),
+  auth = require("../../middleware/auth"),
+  Post = require("../../models/Post"),
+  Profile = require("../../models/Profile"),
+  User = require("../../models/User");
+
+// @route   POST api/post
+// @desc    Create a post
+// @access  Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route   GET api/post
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("posts Route"));
+// @desc    Gets all posts
+// @access  Private
 
 module.exports = router;
