44c5a0c486ca0ca3131a12611ce9499debbd8592
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-06 23:50:45 +0530

    Added route to add likes

--- routes/api/post.js
+++ routes/api/post.js
@@ -99,4 +99,30 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+// @route   Put api/post/like/:id
+// @desc    likes a post
+// @access  Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check for like
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
