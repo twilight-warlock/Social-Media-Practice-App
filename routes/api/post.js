e9be09c9ba33a59cd369cf795e94b67102eccb7e
twilight-warlock <devansh.ts@somaiya.edu>
2020-10-06 21:38:30 +0530

    Added route to get post by passing an id

--- routes/api/post.js
+++ routes/api/post.js
@@ -71,4 +71,32 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+// @route   Delete api/post/:id
+// @desc    Delete a post
+// @access  Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check for user login
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).send("User not authorized");
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
